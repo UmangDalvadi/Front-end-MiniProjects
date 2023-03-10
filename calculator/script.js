2bf3d5107f1df9220ba04168eccd3cac503a3080
UmangDalvadi <umangdalvadi8@gmail.com>
2023-03-11 01:51:54 +0530

    first commit

---
+++ calculator/script.js
@@ -0,0 +1,27 @@
+let btn = document.querySelectorAll('button')
+let input = document.querySelector('input')
+let str = ""
+Array.from(btn).forEach(btn => {
+    btn.addEventListener('click', (btn) => {
+        // console.log(btn.target.innerHTML)
+        if (btn.target.innerHTML == '=') {
+            str = eval(str)
+            input.value = str
+        }
+        else if (btn.target.innerHTML == 'AC') {
+            str = ""
+            input.value = str
+        }
+        else if (btn.target.innerHTML == 'DEL') {
+            str = str.substring(0, str.length - 1)
+            input.value = str
+        }
+
+        else {
+            str = str + btn.target.innerHTML
+            input.value = str
+        }
+    })
+})
+
+
